9e4183845386dacd9faf15a3e7b519d6c1d0d8ea
Kirill2034 <dum15.00@mail.ru>
2019-12-13 21:31:10 +0400

    Initial build subtree commit

---
+++ precache-manifest.fee98b8b6664bb0c3159cef9665c7b7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "976c7435af5e1f94bf78528bda97af2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3aa3a09746e6c65cbe5b",
+    "url": "/static/css/main.cba9d621.chunk.css"
+  },
+  {
+    "revision": "aa26a44db1579f0a1e2b",
+    "url": "/static/js/2.0a717208.chunk.js"
+  },
+  {
+    "revision": "3aa3a09746e6c65cbe5b",
+    "url": "/static/js/main.437d5e45.chunk.js"
+  },
+  {
+    "revision": "52edc29f3077142a659a",
+    "url": "/static/js/runtime-main.ef9d2cf1.js"
+  }
+]);
